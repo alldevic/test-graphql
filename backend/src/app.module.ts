ffe9b353aca965d52d415b7ea5657fd38db19fdf
Nikolay Bely <belyynikolay@gmail.com>
2023-04-14 20:56:45 +0700

    Add todos sample

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+
+import { TodosModule } from './todos/todos.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      typePaths: ['./**/*.graphql'],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+      plugins: [],
     }),
+
+    TodosModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ backend/src/todos/todos.resolver.ts
@@ -0,0 +1,62 @@
+import { Resolver, Args, Int, Mutation, Query } from '@nestjs/graphql';
+import { StatusArgs } from './dtos/args/status.args';
+import { CreateTodoInput, UpdateTodoInput } from './dtos/inputs';
+import { TodoEntity } from './entities/todo.entity';
+import { TodosService } from './todos.service';
+import { AggregationsType } from './types/aggregations.type';
+
+@Resolver(() => TodoEntity)
+export class TodosResolver {
+  constructor(private readonly todoService: TodosService) {}
+
+  @Query(() => [TodoEntity], { name: 'todos' })
+  findAll(@Args() statusArgs: StatusArgs): TodoEntity[] {
+    return this.todoService.findAll(statusArgs);
+  }
+
+  @Query(() => TodoEntity, { name: 'todo' })
+  findOne(@Args('id', { type: () => Int }) id: number) {
+    return this.todoService.findOne(id);
+  }
+
+  @Mutation(() => TodoEntity, { name: 'createTodo' })
+  createTodo(@Args('createTodoInput') createTodoInput: CreateTodoInput) {
+    return this.todoService.create(createTodoInput);
+  }
+
+  @Mutation(() => TodoEntity, { name: 'updateTodo' })
+  updateTodo(@Args('updateTodoInput') updateTodoInput: UpdateTodoInput) {
+    return this.todoService.update(updateTodoInput.id, updateTodoInput);
+  }
+
+  @Mutation(() => Boolean)
+  removeTodo(@Args('id', { type: () => Int }) id: number) {
+    return this.todoService.delete(id);
+  }
+
+  // Aggregations
+  @Query(() => Int, { name: 'totalTodos' })
+  totalTodos(): number {
+    return this.todoService.totalTodos;
+  }
+
+  @Query(() => Int, { name: 'pendingTodos' })
+  pendingTodos(): number {
+    return this.todoService.pendingTodos;
+  }
+
+  @Query(() => Int, { name: 'completedTodos' })
+  completedTodos(): number {
+    return this.todoService.completedTodos;
+  }
+
+  @Query(() => AggregationsType)
+  aggregations(): AggregationsType {
+    return {
+      completed: this.todoService.completedTodos,
+      pending: this.todoService.pendingTodos,
+      total: this.todoService.totalTodos,
+      totalTodosCompleted: this.todoService.totalTodos,
+    };
+  }
+}
